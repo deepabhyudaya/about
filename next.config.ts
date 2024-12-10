24124936a35953c081b64a4c5a8d8de8fd2d5bbe
Abhyudaya <pixeldrift.adv@proton.me>
2024-12-10 21:56:49 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,15 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  // async redirects() {
-  //   return [
-  //     {
-  //       source: '/',
-  //       destination: 'https://github.com/deepabhyudaya',
-  //       permanent: true,
-  //     },
-  //   ]
-  // },
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: 'https://www.youtube.com/watch?v=BK7Tw8uZ8Ss',
+        permanent: true,
+      },
+    ]
+  },
 
   /* config options here */
   images: {
